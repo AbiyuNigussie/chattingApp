0010682146897ee65a1221058e34c6e8819c4a49
AbiyuTolosa <abiyunigussie7@gmail.com>
2022-01-27 16:30:50 -0800

    added User schema

--- server/app.js
+++ server/app.js
@@ -3,14 +3,32 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-require('dotenv');
+require('dotenv/config');
 
-// SERVER PORT
+// Middleware
+
+// Enable cross Origin requests 
+app.use(cors());
+
+// Enables us to read json frmat responses
+app.use(express.json());
+
+// Import Routes
+const authRoute = require('./routes/auth');
+
+// Route Middlewares
+app.use('/api/user', authRoute);
+// Server Port
 const PORT = 3001
 
-// MAIN ROUTE
+// Main Route
 app.get("/", (req, res) => {
     res.send("Web Application Server!")
+});
+
+// CONNECT TO DATABASE
+mongoose.connect(process.env.DB_CONNECTION, () => {
+    console.log("DB Connected!");
 });
 
 app.listen(PORT, () => {
